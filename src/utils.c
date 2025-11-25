32f1e3d89bbbb14ad858abe4ccdf206017dc6622
Ddilibe <franklinfidelugwuowo@gmail.com>
2025-11-25 04:16:52 +0100

    chore: created a persistent grid for tetris blocks

--- src/utils.c
+++ src/utils.c
@@ -1,4 +1,5 @@
 #include "tetris.h"
+#include "logger.h"
 
 void print_2d_array(int **array, int rows, int cols)
 {
@@ -28,10 +29,18 @@ void print_2d_array(int **array, int rows, int cols)
  *          if i%18 == 0:
  *              w += pow(2, i)
  *      return w
- * 
+ *
  *  def display_binary(i: int):
  *      binary = f"{bin(i)[2:]}"
  *      for i in range(23):
  *          print(binary[(18*i-18): (18*i)])
- * 
+ *
  * */
+
+void print_block(Linked_Node *node)
+{
+    printf("INFO: LinkedNode with Address %p\n", node);
+    printf("\tDimension: x->%d, y->%d\n", node->dimension[0], node->dimension[1]);
+    printf("\tColor: %d\n", node->color);
+    LOG_INFO("Printing BLOCK");
+}
